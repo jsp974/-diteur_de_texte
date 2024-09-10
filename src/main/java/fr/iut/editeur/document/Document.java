7577b7b607c964ee6bb8ff926f85cd279a15f21e
hugo bertrand <hugo.bertrand03@etu.umontpellier.fr>
2024-09-10 17:11:30 +0200

    exo 5 debut

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -26,6 +26,10 @@ public class Document {
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    public void majuscules(int debut, int fin) {
+        //TO-DO!
+    }
+
     @Override
     public String toString() {
         return this.texte;
