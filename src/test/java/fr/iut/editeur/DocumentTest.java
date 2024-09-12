cc98429bbf388142d2834622280991900c6a6ce3
hugo bertrand <hugo.bertrand03@etu.umontpellier.fr>
2024-09-12 10:22:10 +0200

    mise en place de tests.yml

---
+++ src/test/java/fr/iut/editeur/DocumentTest.java
@@ -0,0 +1,14 @@
+package fr.iut.editeur;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DocumentTest {
+
+    @Test
+    public void testMethode() {
+        assertTrue(true);
+    }
+
+}
